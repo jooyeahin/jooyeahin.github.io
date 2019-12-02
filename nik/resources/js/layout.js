0fd15924b6b1e69200eb06ff373802963d264d4f
jooyeahin <jooyeahin@naver.com>
2019-12-02 17:23:50 +0900

    Update layout.js

--- nik/resources/js/layout.js
+++ nik/resources/js/layout.js
@@ -356,10 +356,10 @@ $(document).ready(function(){
            slidesPerView: 2,
            slidesPerGroup:1,
          },
-         // 766:{
-         //   slidesPerView: 1,
-         //   slidesPerGroup:1,
-         // },
+         766:{
+           slidesPerView: 1,
+           slidesPerGroup:1,
+         },
        }
     });
 
@@ -411,9 +411,21 @@ $(document).ready(function(){
       );
     }
 
+    // var depth3swiper = new Swiper('.sub_menu .right .swiper-container', {
+    //   init: true,
+    //   slidesPerView:1,
+    //   slidesPerGroup:1,
+    //   nitialSlide: 0,
+    //   allowTouchMove: false,
+    //   normalizeSlideIndex: true,
+    //   navigation: {
+    //     nextEl: '.sub_menu .right .swiper-button-next',
+    //     prevEl: '.sub_menu .right .swiper-button-prev',
+    //   },
+    // });
+
     var depth3swiper = new Swiper('.sub_menu .right .swiper-container', {
-      init: true,
-      slidesPerView:1,
+      slidesPerView:5,
       slidesPerGroup:1,
       nitialSlide: 0,
       allowTouchMove: true,
@@ -422,9 +434,21 @@ $(document).ready(function(){
         nextEl: '.sub_menu .right .swiper-button-next',
         prevEl: '.sub_menu .right .swiper-button-prev',
       },
+      breakpoints: {
+         1199: {
+           slidesPerView: 3,
+           slidesPerGroup:1,
+         },
+         991:{
+           slidesPerView: 2,
+           slidesPerGroup:1,
+         },
+         766:{
+           slidesPerView: 1,
+           slidesPerGroup:1,
+         },
+       }
     });
-
-
 
     var depth2swiper = new Swiper('.sub_menu.'+code+ ' .left .category .swiper-container', {
       slidesPerView:2,
