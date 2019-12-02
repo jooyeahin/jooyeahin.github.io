9f6ca6fe04202ebf6baf459a93c85a1507eb49a6
jooyeahin <jooyeahin@naver.com>
2019-12-02 17:14:33 +0900

    Update layout.js

--- nik/resources/js/layout.js
+++ nik/resources/js/layout.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
-  var depth3swiper = new Swiper('.sub_menu .right .swiper-container', {
-    slidesPerView:5,
-    slidesPerGroup:1,
-    nitialSlide: 0,
-    allowTouchMove: true,
-    normalizeSlideIndex: true,
-    navigation: {
-      nextEl: '.sub_menu .right .swiper-button-next',
-      prevEl: '.sub_menu .right .swiper-button-prev',
-    },
-    breakpoints: {
-       1199: {
-         slidesPerView: 3,
-         slidesPerGroup:1,
-       },
-       991:{
-         slidesPerView: 2,
-         slidesPerGroup:1,
-       },
-       766:{
-         slidesPerView: 1,
-         slidesPerGroup:1,
-       },
-     }
-  });
+  // var depth3swiper = new Swiper('.sub_menu .right .swiper-container', {
+  //   slidesPerView:5,
+  //   slidesPerGroup:1,
+  //   nitialSlide: 0,
+  //   allowTouchMove: true,
+  //   normalizeSlideIndex: true,
+  //   navigation: {
+  //     nextEl: '.sub_menu .right .swiper-button-next',
+  //     prevEl: '.sub_menu .right .swiper-button-prev',
+  //   },
+  //   breakpoints: {
+  //      1199: {
+  //        slidesPerView: 3,
+  //        slidesPerGroup:1,
+  //      },
+  //      991:{
+  //        slidesPerView: 2,
+  //        slidesPerGroup:1,
+  //      },
+  //      766:{
+  //        slidesPerView: 1,
+  //        slidesPerGroup:1,
+  //      },
+  //    }
+  // });
   var productSwiper = new Swiper('.cont_area01 .swiper-container', {
     slidesPerView:5,
     slidesPerGroup:1,
@@ -337,31 +337,31 @@ $(document).ready(function(){
       );
     }
 
-    // var depth3swiper = new Swiper('.sub_menu .right .swiper-container', {
-    //   slidesPerView:5,
-    //   slidesPerGroup:1,
-    //   nitialSlide: 0,
-    //   allowTouchMove: true,
-    //   normalizeSlideIndex: true,
-    //   navigation: {
-    //     nextEl: '.sub_menu .right .swiper-button-next',
-    //     prevEl: '.sub_menu .right .swiper-button-prev',
-    //   },
-    //   breakpoints: {
-    //      1199: {
-    //        slidesPerView: 3,
-    //        slidesPerGroup:1,
-    //      },
-    //      991:{
-    //        slidesPerView: 2,
-    //        slidesPerGroup:1,
-    //      },
-    //      766:{
-    //        slidesPerView: 1,
-    //        slidesPerGroup:1,
-    //      },
-    //    }
-    // });
+    var depth3swiper = new Swiper('.sub_menu .right .swiper-container', {
+      slidesPerView:5,
+      slidesPerGroup:1,
+      nitialSlide: 0,
+      allowTouchMove: true,
+      normalizeSlideIndex: true,
+      navigation: {
+        nextEl: '.sub_menu .right .swiper-button-next',
+        prevEl: '.sub_menu .right .swiper-button-prev',
+      },
+      breakpoints: {
+         1199: {
+           slidesPerView: 3,
+           slidesPerGroup:1,
+         },
+         991:{
+           slidesPerView: 2,
+           slidesPerGroup:1,
+         },
+         766:{
+           slidesPerView: 1,
+           slidesPerGroup:1,
+         },
+       }
+    });
 
   })
 
@@ -412,11 +412,11 @@ $(document).ready(function(){
     }
 
     var depth3swiper = new Swiper('.sub_menu .right .swiper-container', {
-      init: true,
+
       slidesPerView:1,
       slidesPerGroup:1,
       nitialSlide: 0,
-      allowTouchMove: false,
+      // allowTouchMove: false,
       normalizeSlideIndex: true,
       navigation: {
         nextEl: '.sub_menu .right .swiper-button-next',
